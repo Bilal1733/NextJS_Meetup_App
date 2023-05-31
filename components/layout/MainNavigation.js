033c73165a4a934eddc1b2abd1cd592c5f1887f5
Bilal <bks1733@gmail.com>
2023-05-31 12:56:12 +0530

    The "_app.js" File & Layout Wrapper.

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,21 +1,24 @@
+import Link from 'next/link';
 import classes from './MainNavigation.module.css';
+import { Fragment } from 'react';
 
 function MainNavigation() {
-
   return (
-    <header className={classes.header}>
-      <div className={classes.logo}>React Meetups</div>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>All Meetups</Link>
-          </li>
-          <li>
-            <Link to='/new-meetup'>Add New Meetup</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <Fragment>
+      <header className={classes.header}>
+        <div className={classes.logo}>React Meetups</div>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">All Meetups</Link>
+            </li>
+            <li>
+              <Link href="/new-meetup">Add New Meetup</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </Fragment>
   );
 }
 
