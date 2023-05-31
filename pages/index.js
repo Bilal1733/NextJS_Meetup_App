51182c48f58cf9bcb09aedf315dcd1dec3f531af
Bilal <bks1733@gmail.com>
2023-05-31 15:13:07 +0530

    How Pre-rendering Works & Which Problem We Face.

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,34 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
-const HomePage = () => {
-  const DUMMY_MEETUPS = [
-    {
-      id: 'm1',
-      title: 'A First Meetup',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Some address 5, 12345 Some City',
-      description: 'This is a first meetup!',
-    },
-    {
-      id: 'm2',
-      title: 'A Second Meetup',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Some address 10, 12345 Some City',
-      description: 'This is a second meetup!',
-    },
-  ];
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup!',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 10, 12345 Some City',
+    description: 'This is a second meetup!',
+  },
+];
 
+const HomePage = () => {
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+  useEffect(() => {
+    // send http request and fetch data
+    setLoadedMeetups(DUMMY_MEETUPS);
+  }, []);
   return (
     <Fragment>
-      <MeetupList meetups={DUMMY_MEETUPS} />
+      <MeetupList meetups={loadedMeetups} />
     </Fragment>
   );
 };
